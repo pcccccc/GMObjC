5e2cd8f0a41f912af075b1d8a85b06c58d1eeadb
LaoPC <pangchongcyc@gmail.com>
2025-11-04 11:23:21 +0800

    test

--- Package.swift
+++ Package.swift
@@ -14,12 +14,12 @@ let package = Package(
         .binaryTarget(
             name: "openssl",
             url: "https://github.com/pcccccc/GMObjC/releases/download/1.0.7/openssl.xcframework.zip",
-            checksum: "d320f33100d10ed6ccdf67f616801c3b405ffcf4c6d622dd5c5f9369eec0be27"
+            checksum: "c256e92cdadeba038f070229b595b7808a620165324587ecd2437f2dffff8bc7"
         ),
         .binaryTarget(
             name: "GMObjC",
             url: "https://github.com/pcccccc/GMObjC/releases/download/1.0.7/GMObjC.xcframework.zip",
-            checksum: "3405a3ec2af06540d3911fd982600936d44b43a7a03dc24c73050cf3ab386b89"
+            checksum: "56e04a65c4d7a94ac29033f7b1e51d1298cbecf6950cbe064af7f51b84521a20"
         )
     ]
 )
